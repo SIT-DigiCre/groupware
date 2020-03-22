f4aaad3d527e2d0dd5561f0157842011c5d9e751
MogamiTsuchikawa <30793866+MogamiTsuchikawa@users.noreply.github.com>
2020-03-22 21:31:39 +0900

    created indexPage (#7)

--- digigru/settings.py
+++ digigru/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'home',
     'bbs',
     'ringi',
+    'member',
 
     # 3rd-party
     'markdownx',
--- member/views.py
+++ member/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required(login_url='/admin/login/')
+def index(request):
+    # GETアクセス時の処理
+    params = {
+        
+    }
+    return render(request, 'member/index.htm', params)
