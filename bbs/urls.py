7f382f350755a2b1426d31d2dc72b43a489d0e49
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2020-03-24 04:14:25 +0900

    fix #9

--- bbs/urls.py
+++ bbs/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     #path('', views.index, name='index'),
-    path('', views.jump, name='jump'),
-    path('<channel_name>/', views.index, name='index'),
-    path('<channel_name>/show/<int:id>', views.show, name='index'),
-    path('<channel_name>/<int:page>', views.index, name='index'),
+    path('', views.jump, name='bbs.jump'),
+    path('<channel_name>/', views.index, name='bbs.index'),
+    path('<channel_name>/show/<int:id>', views.show, name='bbs.show'),
+    path('<channel_name>/<int:page>', views.index, name='bbs.index'),
 ]
--- ringi/urls.py
+++ ringi/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:page>', views.index, name='index'),
-    path('create', views.create, name='create'),
+    path('', views.index, name='ringi.index'),
+    path('<int:page>', views.index, name='ringi.index'),
+    path('create', views.create, name='ringi.create'),
 ]
