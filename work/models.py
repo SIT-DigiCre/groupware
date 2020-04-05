b6dcc3e1454d2ac360b8dbf39a581742185abce5
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2020-04-06 03:35:51 +0900

    add work models and view

--- work/models.py
+++ work/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from tool.models import Tool
+from account.models import User
+from member.models import UserTool
+from markdownx.models import MarkdownxField
+from markdownx.utils import markdownify
 # Create your models here.
+
+class Work(models.Model):
+    name =  models.CharField(max_length=50)
+    tool = models.ForeignKey(Tool,on_delete=models.CASCADE)
+    user = models.ManyToManyField(User)
+    leader = models.OneToOneField(User,on_delete=models.SET_NULL,null=True)
+    main_image = models.ImageField(upload_to='work_main_image/', default='defo')
+    intro = MarkdownxField('intro')
--- work/views.py
+++ work/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def index(request):
+    pass
+
+def show(request):
+    pass
+
+def edit(request):
+    pass
+
+def create(request):
+    pass
