1cc385cc177cb28f23d5e88d36409990e49f1416
Kaoru Chisen <cordx56@cordx.net>
2020-04-13 23:12:31 +0900

    use mariadb

--- digigru/settings.py
+++ digigru/settings.py
@@ -94,6 +94,14 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+if not DEBUG:
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('MYSQL_DATABASE'),
+        'USER': os.environ.get('MYSQL_USER'),
+        'PASSWORD': os.environ.get('MYSQL_PASSWORD'),
+        'HOST': 'db'
+    }
 
 
 # Password validation
