1e80c1303bef929dece4063ed7e584546ff9ff60
syun560 <al18056@shibaura-it.ac.jp>
2020-04-20 18:20:58 +0900

    新規スレッド作成の処理を別の関数に

--- bbs/urls.py
+++ bbs/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     #path('', views.index, name='index'),
     path('', views.jump, name='bbs.jump'),
+    path('new_thread/', views.new_thread, name='bbs.new_thread'),
     path('<channel_name>/', views.index, name='bbs.index'),
     path('<channel_name>/show/<int:id>', views.show, name='bbs.show'),
     path('<channel_name>/<int:page>', views.index, name='bbs.index'),
