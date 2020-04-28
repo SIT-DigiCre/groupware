795845720a3bedc40d56f6107bf5a716796c17b5
MogamiTsuchikawa <30793866+MogamiTsuchikawa@users.noreply.github.com>
2020-04-28 17:04:40 +0900

    Blog app (#24)
    * add models and urls
    
    * add index show create page
    
    * add edit func and login menu
    
    * fix logout and login
    
    * blogのtag関連の処理を追加

--- blog/urls.py
+++ blog/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
     path('article/<int:id>', views.show, name='blog.show'),
     path('article/<int:id>/edit', views.edit, name='blog.edit'),
     path('article/create', views.create, name='blog.create'),
+    path('tag/create',views.create_tag,name='blog.create_tag'),
+    path('tag/<int:id>',views.show_tag,name='blog.show_tag'),
+    path('tag/<int:id>/edit',views.edit_tag,name='blog.edit_tag'),
+    path('tag',views.index_tag,name='blog.index_tag'),
+    path('article/<int:id>/tags',views.edit_art_tags,name='blog.edit_art_tags'),
 ]
