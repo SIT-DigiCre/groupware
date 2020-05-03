054ebe13d94e07cc98d81d1f2943e65724fcbdfc
MogamiTsuchikawa <30793866+MogamiTsuchikawa@users.noreply.github.com>
2020-05-04 00:39:16 +0900

    Member work tool enhance (#36)
    * change usertool tag at usertool.htm
    
    * fix work.show
    
    * 何期かのタグや班の登録機能の追加・班やツール追加のフォームの改良・メンバー詳細ページのデザインを良さげにした
    
    * add usertool のバグを修正

--- member/views.py
+++ member/views.py
@@ -32,13 +32,21 @@ def edit(request):
     # POSTアクセス時（返信時）の処理
     if request.method == 'POST':
         if 'add-usertool-form' in request.POST:
-            usertool_form = UserToolForm(request.POST)
-            if usertool_form.is_valid():
+            toolid = request.POST['toolchoice']
+            tool = Tool.objects.filter(id=toolid).first()
+            usertool = UserTool.objects.filter(tool=tool,profile=profile)
+            if len(usertool.all()) == 0:
                 usertool = UserTool()
-                usertool.level = usertool_form.cleaned_data['level']
-                usertool.tool = usertool_form.cleaned_data['tool']
-                usertool.profile = profile
-                usertool.save()
+            else:
+                usertool = usertool.first()
+            usertool.profile = profile
+            usertool.tool = tool
+            usertool.level = int(request.POST['toollevel'])
+            usertool.save()
+        if 'add-division-form' in request.POST:
+            divisionid = int(request.POST['divisionchoice'])
+            division = Division.objects.filter(id=divisionid).first()
+            profile.divisions.add(division)
         if 'user-edit-form' in request.POST:
             user_form = UserEditForm(request.POST,request.FILES,instance = request.user)
             user_form.save()
@@ -48,11 +56,19 @@ def edit(request):
         
         # 同じページにリダイレクトするだけなので、もっと賢い書き方あるはず
         return redirect(to='/member/edit' )
+    exist_tools = []
+    for usertool in profile.usertool_set.all():
+        exist_tools.append(usertool.tool)
+    
     # GETアクセス時の処理
     params = {
         'add_usertool_form': UserToolForm(),
         'user_form':UserEditForm(instance=request.user),
-        'profile_form':ProfileForm(instance=request.user.profile)
+        'profile_form':ProfileForm(instance=request.user.profile),
+        'tools':Tool.objects.all(),
+        'exist_tools':exist_tools,
+        'divisions':Division.objects.all(),
+        'profile':profile,
     }
     return render(request, 'member/edit.htm', params)
 
