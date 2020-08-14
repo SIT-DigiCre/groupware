1104b309637c6aa1c1ef2508687bcbd90d6eda44
task4233 <tks.m1205@gmail.com>
2020-08-15 00:03:59 +0900

    add completed test

--- issue/tests.py
+++ issue/tests.py
@@ -44,26 +44,141 @@ class IssuePageTest(TestCase):
         response = client.get('/issue/create')
         self.assertEqual(response.status_code, 200)        
 
-        # TODO: 
-        # extension in category is temporarily added in my environment.
-        # please update this information along with set category.
+        # send post request
         data = {
-            "title": "test",
-            "category": "test category",
+            "title": "test data",
+            "category": "1", # number is set by fixture
             "content": "test content"
         }
         response = client.post('/issue/create', data)
-        self.assertRedirects(response, '/issue/', status_code=302, target_status=200, msg_prefix='', fetch_redirect_response=True)
+
+        # check redirects
+        self.assertRedirects(response, '/issue/', status_code=302,
+                             target_status_code=200, msg_prefix='',
+                             fetch_redirect_response=True)
+        
+        # check if it's registered
+        response = client.get('/issue/')
+        self.assertIn(data["title"], response.content.decode())
+        self.assertIn(data["category"], response.content.decode())
+        self.assertIn(data["content"], response.content.decode())
+
+    def test_create_issue_with_invalid_data(self):
+        """
+        tests that it can create issue
+        and it will have been registered.
+        """
+        # make authenticated user
+        user, client = self.make_authenticated_user()
+        self.assertTrue(user.is_active, True)
+
+        # send post request
+        data = {
+            "title": "test data",
+            "category": "-1", # invalid
+            "content": "test content"
+        }
+        response = client.post('/issue/create', data)
+
+        # check redirects
+        self.assertRedirects(response, '/issue/', status_code=302,
+                             target_status_code=200, msg_prefix='',
+                             fetch_redirect_response=True)
+        
+        # check if it's registered
+        response = client.get('/issue/')
+        self.assertNotIn(data["title"], response.content.decode())
+        self.assertNotIn(data["content"], response.content.decode())
+
+    def test_delete_issue(self):
+        # make authenticated superuser
+        user, client = self.make_authenticated_superuser()
+        self.assertTrue(user.is_active, True)
+
+        # send post request
+        data = {
+            "title": "test data",
+            "category": "1", # number is set by fixture
+            "content": "test content"
+        }
+        response = client.post('/issue/create', data)
+        response = client.get('/issue/')
+
+        # check if exists delete link
+        self.assertContains(response, 'href="{0}"'.format('/issue/edit/1'))
+
+        response = client.get('/issue/edit/1')
+
+        # send post request
+        data = {
+            "title": "updated test data",
+            "category": "1", # number is set by fixture
+            "content": "updated test content"
+        }
+        self.assertContains(response, 'action="{0}"'.format('/issue/edit/1'))
+        response = client.post('/issue/edit/1', data)
+        
+        self.assertRedirects(response, '/issue/', status_code=302,
+                             target_status_code=200, msg_prefix='',
+                             fetch_redirect_response=True)
+
+        
+    def test_delete_issue(self):
+        # make authenticated superuser
+        user, client = self.make_authenticated_superuser()
+        self.assertTrue(user.is_active, True)
+
+        # send post request
+        data = {
+            "title": "test data",
+            "category": "1", # number is set by fixture
+            "content": "test content"
+        }
+        response = client.post('/issue/create', data)
+        response = client.get('/issue/')
+
+        # check if exists delete link
+        self.assertContains(response, 'href="{0}"'.format('/issue/delete/1'))
+
+        response = client.get('/issue/delete/1')
+        self.assertContains(response, 'action="{0}"'.format('/issue/delete/1'))
+        response = client.post('/issue/delete/1')
+        
+        self.assertRedirects(response, '/issue/', status_code=302,
+                             target_status_code=200, msg_prefix='',
+                             fetch_redirect_response=True)
+    
         
     def make_authenticated_user(self):
         """
         makes authenticated user for testing
         """
 
-        user = User.objects.create_user(self.test_user["username"],self.test_user["email"], self.test_user["last_name"])
+        user = User.objects.create_user(self.test_user["username"],
+                                        self.test_user["email"],
+                                        self.test_user["last_name"])
         user.first_name = self.test_user["first_name"]
         user.student_id = self.test_user["student_id"]
         user.is_active = True
+        user.save()
+
+        client = Client()
+        client.force_login(user)
+        return user, client
+
+    
+    def make_authenticated_superuser(self):
+        """
+        makes authenticated user for testing
+        """
+
+        user = User.objects.create_user(self.test_user["username"],
+                                        self.test_user["email"],
+                                        self.test_user["last_name"])
+        user.first_name = self.test_user["first_name"]
+        user.student_id = self.test_user["student_id"]
+        user.is_active = True
+        user.is_superuser = True
         user.save()
 
         client = Client()
