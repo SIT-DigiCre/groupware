653f24d7265dc159ad4af279f4970f8cf5d9412d
syun560 <al18056@shibaura-it.ac.jp>
2020-08-15 09:50:51 +0900

    import忘れ
    Co-authored-by: task4233 <tks.m1205@gmail.com>

--- issue/views.py
+++ issue/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
+from account.models import User
 from django.contrib.auth.decorators import login_required, permission_required
 from django.core.paginator import Paginator
 from django.db.models import Count
