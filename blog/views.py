71d001c643a28f019a12487dec792a2c8884e634
syun560 <al18056@shibaura-it.ac.jp>
2020-08-18 17:31:24 +0900

    Update blog/views.py
    Co-authored-by: task4233 <tks.m1205@gmail.com>

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
-from account.models import User
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.db.models import Q
