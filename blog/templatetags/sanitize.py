466335dde31276b3ac375fccdb7631d9bb25ac74
syun560 <al18056@shibaura-it.ac.jp>
2020-12-03 22:08:59 +0900

    置換する文字列を変更

--- blog/templatetags/sanitize.py
+++ blog/templatetags/sanitize.py
@@ -5,4 +5,4 @@ register = template.Library()
 # scriptを置換して無効化するフィルタ
 @register.filter
 def sanitize(value):
-    return value.replace('script', 'pre').replace('style', 'pre')
+    return value.replace('<script', '<pre').replace('</script', '</pre').replace('<style', '<pre').replace('</style', '</pre')
