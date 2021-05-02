8bf8b78cf21c32bb6d45aedf24a4911dd6465533
task4233 <29667656+task4233@users.noreply.github.com>
2021-05-02 20:15:48 +0900

    refactor: コメントのwhitespaceを追加

--- bbs/urls.py
+++ bbs/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #path('', views.index, name='index'),
+    # path('', views.index, name='index'),
     path('', views.jump, name='bbs.jump'),
     path('<channel_name>/', views.index, name='bbs.index'),
     path('<channel_name>/show/<int:id>', views.show, name='bbs.show'),
