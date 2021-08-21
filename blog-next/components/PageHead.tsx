8c157b1ccb6e6de268ec9002cc2bccd09bcf4446
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2021-08-22 04:18:28 +0900

    デジクリブログに名称を変更、見た目を修正

--- blog-next/components/PageHead.tsx
+++ blog-next/components/PageHead.tsx
@@ -13,11 +13,12 @@ const PageHead = ({title,description,img}:PageHeadProps) => {
 	}
 	return (
 		<Head>
+			<title>{title}</title>
 			<meta name="description" content={title} />
 			<meta property="og:type" content="website" />
 			<meta property="og:title" content={title} />
 			<meta property="og:description" content={text} />
-      {img === undefined?<div></div>:<meta property="og:image" content={img} />}
+			{img === undefined?<div></div>:<meta property="og:image" content={img} />}
 		</Head>
 	)
 }
