ff9accf5638ed760f598db32d5abd45db312921f
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2021-08-28 16:57:27 +0900

    実装（CORS問題あり）

--- blog-next/pages/index.tsx
+++ blog-next/pages/index.tsx
@@ -9,11 +9,20 @@ import PageHead from '../components/PageHead';
 import { getPreviewText } from '../utils/markdown-util'
 import { baseURL } from '../utils/common'
 import { useState, useEffect } from 'react'
+import InfiniteScroll from "react-infinite-scroller"
 
 const IndexPage = (props: { data: ArticleList }) => {
   const [articles, setArticles] = useState<Article[]>(props.data.results)
-  let articlePreviousUrl = props.data.previous
-  // react-infinite-scrollerを使用する
+  let articleNextUrl = props.data.next
+  console.log(articleNextUrl)
+  const loader = <div className="loader" key={0}>Loading ...</div>;
+  const loadNext = async () => {
+    console.log(articleNextUrl)
+    const resData = axios.get(articleNextUrl);
+    const data: ArticleList = (await resData).data;
+    articleNextUrl = data.next
+    setArticles(articles.concat(data.results))
+  }
   return (
     <div>
       <PageHead title='デジクリブログ' description='芝浦工業大学 デジクリのブログサイト' />
@@ -22,19 +31,26 @@ const IndexPage = (props: { data: ArticleList }) => {
           <h3>デジクリブログ最新記事</h3>
         </Row>
         <Row>
-          {articles.map(article => (
-            <Col md={4} sm={6} className='mt-2'>
-              <Link href={'/article/' + String(article.id)}>
+          <InfiniteScroll
+            loadMore={loadNext}
+            hasMore={articleNextUrl !== null}
+            loading={loader}
+            style={{ display: 'inline-block' }}
+          >
+            {articles.map(article => (
+              <Col md={4} sm={6} className='mt-2'>
+                <Link href={'/article/' + String(article.id)}>
                   <Card>
-                  <Card.Img src={article.article_image !== '' ? article.article_image : `${baseURL}/blog/article/${article.id}/ogp_image`} height={200} style={{objectFit:'cover'}}></Card.Img>
+                    <Card.Img src={article.article_image !== '' ? article.article_image : `${baseURL}/blog/article/${article.id}/ogp_image`} height={200} style={{ objectFit: 'cover' }}></Card.Img>
                     <Card.Body>
                       <Card.Title>{article.title}</Card.Title>
                       <Card.Text>{getPreviewText(article.content, 120)}</Card.Text>
                     </Card.Body>
                   </Card>
                 </Link>
-            </Col>
-          ))}
+              </Col>
+            ))}
+          </InfiniteScroll>
         </Row>
       </Container>
     </div>
