fd8fd6cf980141f98245144a6fd92db74376b532
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2021-09-02 02:46:28 +0900

    Blog関連ページを追加

---
+++ core-next/pages/blog/index.tsx
@@ -0,0 +1,12 @@
+import { ReactEventHandler, useState } from 'react';
+import { Container, Col, Row } from 'react-bootstrap';
+
+const BlogIndexPage = () => {
+
+  return (
+    <Container>
+    </Container>
+  );
+}
+
+export default BlogIndexPage;
---
+++ core-next/pages/blog/myarticle/index.tsx
@@ -0,0 +1,12 @@
+import { ReactEventHandler, useState } from 'react';
+import { Container, Col, Row } from 'react-bootstrap';
+
+const MyArticleIndexPage = () => {
+
+  return (
+    <Container>
+    </Container>
+  );
+}
+
+export default MyArticleIndexPage;
