82ae83e5a6b139d122fcd53cea02954bc84ab2ea
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2021-09-02 04:23:34 +0900

    edit画面の実装を開始

---
+++ core-next/pages/blog/edit.tsx
@@ -0,0 +1,12 @@
+import { ReactEventHandler, useState } from 'react';
+import { Container, Col, Row } from 'react-bootstrap';
+
+const BlogEditPage = () => {
+
+  return (
+    <Container>
+    </Container>
+  );
+}
+
+export default BlogEditPage;
