81f35ae8ce0402e6746a69f88f4799df7a1590ad
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2021-09-07 03:27:22 +0900

    リセットCSSの採用

--- core-next/pages/_app.tsx
+++ core-next/pages/_app.tsx
@@ -4,15 +4,12 @@ import { Component } from 'react';
 import Drawer from '../components/Drawer';
 import 'highlightjs/styles/vs2015.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-//import '../styles/common.css'
+import '../styles/common.css'
 
 const App = ({ Component, pageProps }) => {
   return (
     <div>
       <Head>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Noto+Sans+JP&subset=japanese" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
       </Head>
       <Drawer>
         <Component {...pageProps} />
