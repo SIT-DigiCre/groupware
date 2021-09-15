f70432d8fc9f1583bcf65c6e51762c3d05655cce
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2021-09-16 03:21:09 +0900

    コード整形

--- core-next/components/Drawer.tsx
+++ core-next/components/Drawer.tsx
@@ -1,49 +1,52 @@
-import Link from 'next/link'
-import { useState } from 'react';
-import Drawer from '@material-ui/core/Drawer';
-import AppBar from '@material-ui/core/AppBar';
-import Badge from '@material-ui/core/Badge';
-import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import Typography from '@material-ui/core/Typography';
-import DescriptionIcon from '@material-ui/icons/Description';
-import CreateIcon from '@material-ui/icons/Create';
-import ChatIcon from '@material-ui/icons/Chat';
-import SettingsIcon from '@material-ui/icons/Settings';
-import ExitToAppIcon from '@material-ui/icons/ExitToApp';
-import GroupIcon from '@material-ui/icons/Group';
-import AttachMoneyIcon from '@material-ui/icons/AttachMoney';
-import NotificationsIcon from '@material-ui/icons/Notifications';
+import Link from "next/link";
+import { useState } from "react";
+import Drawer from "@material-ui/core/Drawer";
+import AppBar from "@material-ui/core/AppBar";
+import Badge from "@material-ui/core/Badge";
+import Toolbar from "@material-ui/core/Toolbar";
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import Typography from "@material-ui/core/Typography";
+import DescriptionIcon from "@material-ui/icons/Description";
+import CreateIcon from "@material-ui/icons/Create";
+import ChatIcon from "@material-ui/icons/Chat";
+import SettingsIcon from "@material-ui/icons/Settings";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import GroupIcon from "@material-ui/icons/Group";
+import AttachMoneyIcon from "@material-ui/icons/AttachMoney";
+import NotificationsIcon from "@material-ui/icons/Notifications";
 
 const MiniDrawer = (props) => {
   const [open, setOpen] = useState(false);
-  const toggleDrawer = (open: boolean) => (
-    event: React.KeyboardEvent | React.MouseEvent,
-  ) => {
-    if (
-      event.type === 'keydown' &&
-      ((event as React.KeyboardEvent).key === 'Tab' ||
-        (event as React.KeyboardEvent).key === 'Shift')
-    ) {
-      return;
-    }
+  const toggleDrawer =
+    (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event.type === "keydown" &&
+        ((event as React.KeyboardEvent).key === "Tab" ||
+          (event as React.KeyboardEvent).key === "Shift")
+      ) {
+        return;
+      }
 
-    setOpen(open);
-  };
+      setOpen(open);
+    };
   return (
     <div>
-      <AppBar position='static'>
+      <AppBar position="static">
         <Toolbar>
-          <IconButton onClick={() => { setOpen(true) }}>
-            <MenuIcon style={{ color: 'white' }} />
+          <IconButton
+            onClick={() => {
+              setOpen(true);
+            }}
+          >
+            <MenuIcon style={{ color: "white" }} />
           </IconButton>
-          <Typography variant='h6' style={{ marginLeft: '5px' }}>
+          <Typography variant="h6" style={{ marginLeft: "5px" }}>
             ﾃﾞｼﾞｺｱ2.0ﾌﾟﾛﾄﾀｲﾌﾟ
           </Typography>
           <div style={{ flexGrow: 1 }}></div>
@@ -54,60 +57,60 @@ const MiniDrawer = (props) => {
           </IconButton>
         </Toolbar>
       </AppBar>
-      <Drawer anchor='left' open={open} onClose={toggleDrawer(false)}>
+      <Drawer anchor="left" open={open} onClose={toggleDrawer(false)}>
         <List>
-          <ListItem button key='chat'>
+          <ListItem button key="chat">
             <ListItemIcon>
               <ChatIcon />
             </ListItemIcon>
-            <ListItemText primary='Chat' />
+            <ListItemText primary="Chat" />
           </ListItem>
-          <ListItem button key='ringi'>
+          <ListItem button key="ringi">
             <ListItemIcon>
               <AttachMoneyIcon />
             </ListItemIcon>
-            <ListItemText primary='Ringi' />
+            <ListItemText primary="Ringi" />
           </ListItem>
-          <ListItem button key='work'>
+          <ListItem button key="work">
             <ListItemIcon>
               <CreateIcon />
             </ListItemIcon>
-            <ListItemText primary='Work' />
+            <ListItemText primary="Work" />
           </ListItem>
-          <ListItem button key='member'>
+          <ListItem button key="member">
             <ListItemIcon>
               <GroupIcon />
             </ListItemIcon>
-            <ListItemText primary='Member' />
+            <ListItemText primary="Member" />
           </ListItem>
-          <Link href='/blog'>
-            <ListItem button key='blog'>
+          <Link href="/blog">
+            <ListItem button key="blog">
               <ListItemIcon>
                 <DescriptionIcon />
               </ListItemIcon>
-              <ListItemText primary='Blog' />
+              <ListItemText primary="Blog" />
             </ListItem>
           </Link>
         </List>
         <Divider />
         <List>
-          <ListItem button key='setting'>
+          <ListItem button key="setting">
             <ListItemIcon>
               <SettingsIcon />
             </ListItemIcon>
-            <ListItemText primary='Setting' />
+            <ListItemText primary="Setting" />
           </ListItem>
-          <ListItem button key='logout'>
+          <ListItem button key="logout">
             <ListItemIcon>
               <ExitToAppIcon />
             </ListItemIcon>
-            <ListItemText primary='Logout' />
+            <ListItemText primary="Logout" />
           </ListItem>
         </List>
       </Drawer>
+      {props.children}
     </div>
-
   );
-}
+};
 
 export default MiniDrawer;
