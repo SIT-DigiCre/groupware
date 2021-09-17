d64d664ab777aec8fad4b5d7eef27fe17e623f96
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2021-09-18 04:24:09 +0900

    UserとMember用でPermissionの修正

--- backend/home/permissions.py
+++ backend/home/permissions.py
@@ -2,7 +2,7 @@
 from rest_framework import permissions
 
 
-class IsOwnerOrReadOnly(permissions.BasePermission):
+class IsOwnerOrReadOnlyMember(permissions.BasePermission):
     """
     Object-level permission to only allow owners of an object to edit it.
     Assumes the model instance has an `owner` attribute.
@@ -16,3 +16,19 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
 
         # Instance must have an attribute named `owner`.
         return obj.member == request.user
+
+
+class IsOwnerOrReadOnlyUser(permissions.BasePermission):
+    """
+    Object-level permission to only allow owners of an object to edit it.
+    Assumes the model instance has an `owner` attribute.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Instance must have an attribute named `owner`.
+        return obj.user == request.user
