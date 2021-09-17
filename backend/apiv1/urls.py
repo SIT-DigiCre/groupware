97c398b0520f07f11b3bf697220387323bd33e6a
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2021-09-18 04:25:50 +0900

    WorkのAPIのURLを追加

--- backend/apiv1/urls.py
+++ backend/apiv1/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import include
 from django.urls import path
 from blog import views as blog_views
 from strage import views as strage_views
+from work import views as work_views
 from rest_framework import routers
 
 blog_router = routers.DefaultRouter()
@@ -12,8 +13,13 @@ blog_router.register('article_tag', blog_views.ArticleTagViewSet)
 strage_router = routers.DefaultRouter()
 strage_router.register('fileobject', strage_views.FileObjectViewSet)
 
+work_router = routers.DefaultRouter()
+work_router.register('item', work_views.WorkItemViewSet)
+work_router.register('tag', work_views.WorkTagViewSet)
+
 urlpatterns = [
     path('blog/', include(blog_router.urls)),
     path('strage/', include(strage_router.urls)),
-    path('strage/fileobject/upload', strage_views.UploadFileObjectView.as_view())
+    path('strage/fileobject/upload', strage_views.UploadFileObjectView.as_view()),
+    path('work/', include(work_router.urls)),
 ]
