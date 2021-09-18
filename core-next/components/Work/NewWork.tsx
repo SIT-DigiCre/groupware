763662b26b4e44fd7a67a0459964e0ea33b4cad5
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2021-09-18 15:50:27 +0900

    WorkItemのPOST処理まで完成

--- core-next/components/Work/NewWork.tsx
+++ core-next/components/Work/NewWork.tsx
@@ -2,22 +2,45 @@ import { Row, Col } from "react-bootstrap";
 import { TextField, Button } from "@material-ui/core";
 import { CloudUpload } from "@material-ui/icons";
 import { Tool } from "../../interfaces/tool";
-import { useState } from "react";
-import { WorkTag } from "../../interfaces/work";
+import React, { useState } from "react";
+import { WorkTag, WorkItem } from "../../interfaces/work";
 import { FileObject } from "../../interfaces/strage";
 import UploadFile from "../Strage/UploadFile";
 import FilePreview from "../Strage/FilePreview";
+import SaveIcon from "@material-ui/icons/Save";
+import { axios } from "../../utils/axios";
 const NewWork = () => {
   const [tools, setTools] = useState<Tool[]>([]);
   const [tags, setTags] = useState<WorkTag[]>([]);
   const [files, setFiles] = useState<FileObject[]>([]);
+  const [nameField, setNameField] = useState("");
+  const [introField, setIntroField] = useState("");
+  const handleOnChangeNameField = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setNameField(e.target.value);
+  const handleOnChangeIntroField = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setIntroField(e.target.value);
 
   const onUploaded = (fileObject: FileObject) => {
     console.log(fileObject.file_name);
-    const tmpFiles = files;
-    tmpFiles.push(fileObject);
     setFiles([...files, fileObject]);
-    console.log(tmpFiles);
+  };
+  const onSave = () => {
+    const fs = files.map((file) => console.log(file.id));
+    console.log(fs);
+    const item: WorkItem = {
+      name: nameField,
+      intro: introField,
+      user: 1,
+      tools: [],
+      tags: [],
+      files: files.map((file) => file.id),
+    };
+    console.log(item);
+    axios.post("/v1/work/item/", item, {
+      headers: {
+        Authorization: "JWT " + localStorage.getItem("jwt"),
+      },
+    });
   };
   return (
     <>
@@ -25,19 +48,46 @@ const NewWork = () => {
         <h1>New Work</h1>
       </Row>
       <Row>
-        <TextField required label="作品名" fullWidth />
+        <TextField
+          required
+          label="作品名"
+          fullWidth
+          onChange={handleOnChangeNameField}
+        />
       </Row>
       <Row>
-        <TextField required label="作品説明" fullWidth multiline maxRows={4} />
+        <TextField
+          required
+          label="作品説明"
+          fullWidth
+          multiline
+          maxRows={4}
+          onChange={handleOnChangeIntroField}
+        />
       </Row>
       <Row>
         {files.map((file) => (
-          <FilePreview fileKind={file.kind} fileUrl={file.file_url} />
+          <FilePreview
+            fileKind={file.kind}
+            fileUrl={file.file_url}
+            fileName={file.file_name}
+          />
         ))}
       </Row>
 
       <Row className="mt-2">
         <UploadFile onUploaded={onUploaded} targetContainer="work-item" />
+      </Row>
+      <Row className="mt-2">
+        <Button
+          variant="contained"
+          color="primary"
+          size="large"
+          startIcon={<SaveIcon />}
+          onClick={onSave}
+        >
+          Save
+        </Button>
       </Row>
     </>
   );
