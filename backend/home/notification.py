9b2d104c699f3e191446dfafbbe354f7a3d86636
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2021-10-07 13:48:19 +0900

    HTMLメールからTEXTメールへ変更

--- backend/home/notification.py
+++ backend/home/notification.py
@@ -1,6 +1,7 @@
 import sendgrid
 from sendgrid.helpers.mail.email import Email
 from sendgrid.sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import To
 from digigru.local_settings import DEFAULT_FROM_EMAIL, SENDGRID_APIKEY
 import os
 from sendgrid.helpers.mail import *
@@ -9,8 +10,8 @@ sg = SendGridAPIClient(api_key=SENDGRID_APIKEY)
 from_email = Email(DEFAULT_FROM_EMAIL)
 
 def send_email(subject, message, to_email):
-    target = Email(to_email)
-    content = Content('text/html', message)
-    mail = Mail(from_email, subject, target, content)
+    target = To(to_email)
+    # content = Content('text/html', message)
+    mail = Mail(from_email=from_email, to_emails=[target], subject=subject, plain_text_content=message)
     response = sg.client.mail.send.post(request_body=mail.get())
     print('SendGrid response:' + str(response.status_code))
