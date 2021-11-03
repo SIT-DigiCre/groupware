49a5f7c201360bc777746bef9f31001afe1d398d
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2021-11-04 01:25:27 +0900

    MyStorageページへのリンクを追加

--- core-next/pages/setting/index.tsx
+++ core-next/pages/setting/index.tsx
@@ -8,7 +8,7 @@ const SettingIndexPage = () => {
     <Grid container alignItems="center" justifyContent="center">
       <Grid item xs={12} sm={11}>
         <h1>Setting</h1>
-        <Button href="/s"></Button>
+        <Button href="/storage">ストレージの管理</Button>
       </Grid>
     </Grid>
   );
