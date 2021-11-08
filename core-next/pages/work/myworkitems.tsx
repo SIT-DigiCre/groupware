1905361cc4363b86f159b9dbc0f7adc144b0b662
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2021-11-08 20:34:38 +0900

    MyWorkItemPageを追加

---
+++ core-next/pages/work/myworkitems.tsx
@@ -0,0 +1,11 @@
+import { Grid } from "@mui/material";
+const MyWorkItemsPage = () => {
+  return (
+    <Grid container alignItems="center" justifyContent="center">
+      <Grid item xs={11}>
+        <h1>My Work Items</h1>
+        
+      </Grid>
+    </Grid>);
+}
+export default MyWorkItemsPage;
